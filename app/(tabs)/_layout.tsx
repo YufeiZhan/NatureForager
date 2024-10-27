bf7da92287ae626f70ecd5f516040a6dca473c9e
Yufei Zhan <selinazhan@Yufeis-MacBook-Pro.local>
2024-10-27 17:53:32 -0400

    Update tab icons and tab shape

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,54 @@
 import { Tabs } from "expo-router";
+import { StyleSheet, Image } from "react-native";
 
 export default function AppLayout(){
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarStyle: {
+                    borderRadius: 35,
+                }
+            }}
+        >
             <Tabs.Screen 
                 name="home"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image source={require('@/assets/tab/foraging-tab-icon.png')} style={styles.icon}/>
+                      ),
+                }}
             />
             <Tabs.Screen 
                 name="profile"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image source={require('@/assets/tab/profile-tab-icon.png')} style={styles.icon}/>
+                      ),
+                }}
             />
             <Tabs.Screen 
                 name="recognition"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image source={require('@/assets/tab/camera-tab-icon.png')} style={styles.icon}/>
+                      ),
+                }}
             />
         </Tabs>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    icon: {
+      width: 35,     // Set the width of the icon
+      height: 35,    // Set the height of the icon
+      resizeMode: 'contain', // Optional: controls how the image fits
+    },
+  });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import React from "react";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout(){
+    const mode = useColorScheme()
+
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="PlantInfoModal" options={{ presentation: "modal" }} />
-        </Stack>
+        <ThemeProvider value={mode === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="PlantInfoModal" options={{ presentation: "modal" }} />
+            </Stack>
+        </ThemeProvider>
     )
 }
