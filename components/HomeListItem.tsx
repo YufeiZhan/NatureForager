ee8d3b16cb072c7de19da4b66f87ff3aec4b2d36
eamonma2001 <yimeng.ma@vanderbilt.edu>
2024-12-01 00:28:09 -0500

    change icons on add reminder screen

--- components/HomeListItem.tsx
+++ components/HomeListItem.tsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { LocationContext } from "@/hooks/LocationContext";
 import { useRouter } from "expo-router";
 import { pureWhite } from "@/constants/Colors";
+import { getTypeIcons } from "@/scripts/getTypeIcons";
 
 interface ItemData {
   taxonId: number;
@@ -14,45 +15,7 @@ interface ItemData {
 
 export default function HomeListItem(item: ItemData) {
   const { location, setLocation } = useContext(LocationContext);
-  const router = useRouter();
-
-  const getTypeIcons = (type: string): any[] => {
-    const typeIconMap: { [key: string]: any } = {
-      leaf: require("@/assets/plant/leaf.png"),
-      flower: require("@/assets/plant/flower.png"),
-      fruit: require("@/assets/plant/fruit.png"),
-      nut: require("@/assets/plant/nut.png"),
-      pod: require("@/assets/plant/pod.png"),
-      pollen: require("@/assets/plant/pollen.png"),
-      root: require("@/assets/plant/root.png"),
-      seed: require("@/assets/plant/seed.png"),
-      shoot: require("@/assets/plant/shoot.png"),
-      tea: require("@/assets/plant/tea.png"),
-      tuber: require("@/assets/plant/tuber.png"),
-    };
-  
-    // For types that share icons with other types
-    const aliases: { [key: string]: string } = {
-      berry: "fruit",
-      sap: "tea",
-    };
-  
-    const icons: any[] = [];
-
-  const typeWords = type.toLowerCase().split(/[\s,]+/);
-
-  // Check each type word against the keys in the typeIconMap and aliases
-  typeWords.forEach((word) => {
-    if (typeIconMap[word]) {
-      icons.push(typeIconMap[word]);
-    } else if (aliases[word]) {
-      icons.push(typeIconMap[aliases[word]]);
-    }
-  });
-
-  return icons;
-  };
-  
+  const router = useRouter();  
 
   return (
     <Pressable
